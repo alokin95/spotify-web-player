8f723e898bdc1ee655bb1f537cd010aff55b7054
nikola.mitrovic <nikola.mitrovic@soprex.com>
2020-08-06 18:30:14 +0200

    init

---
+++ resources/js/routes.js
@@ -0,0 +1,12 @@
+import Home from "./components/HomeComponent";
+
+export default {
+    mode: 'history',
+
+    routes: [
+        {
+            path: '/',
+            component: Home
+        }
+    ]
+}
